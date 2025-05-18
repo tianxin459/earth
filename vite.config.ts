82bc38d196b7d371b2936bf64646e9ced03a0400
Ellis Tian <onlyheart@qq.com>
2025-05-18 16:26:32 +0800

    vite 配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  base: '/earth/', // 设置构建后的基础路径
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/earth/' : '/', // 仅生产环境添加 base
   // ...其他配置
-});
+}));
