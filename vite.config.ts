6f3e3655e40ac570498acc6138e8c3f7f233f766
Ellis Tian <onlyheart@qq.com>
2025-05-19 21:21:09 +0800

    CICD

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/' : '/', // 仅生产环境添加 base
+  base: mode === 'production' ? '/earth/' : '/', // 仅生产环境添加 base
   // ...其他配置
 }));
