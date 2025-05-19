6283c2d9249fea1f0a574ada78774f9eb1ed3de9
Ellis Tian <Ellis.Tian@walmart.com>
2025-05-19 13:58:25 +0800

    wmt deploy: GSpoWorld

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/' : '/', // 仅生产环境添加 base
+  base: mode === 'production' ? '/GSpoWorld/' : '/', // 仅生产环境添加 base
   // ...其他配置
 }));
