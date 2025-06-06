b6222462841e9d68c571e03768c26546b01cafc7
Fletcher Yuan <fletcher.yuan@walmart.com>
2025-06-06 13:44:43 +0800

    添加录制模式

--- src/components/globe/settings/init.ts
+++ src/components/globe/settings/init.ts
@@ -2,6 +2,8 @@ import Globe from "globe.gl";
 
 export const initGlobe = (el: HTMLElement) => {
     return new Globe(el, {})
+        .width(el.clientWidth)
+        .height(el.clientHeight)
         .pointOfView({ lat: 39.6, lng: -98.5, altitude: 2 })
         .showAtmosphere(true)
         .atmosphereAltitude(0.25);
